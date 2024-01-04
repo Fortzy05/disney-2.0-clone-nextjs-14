2b357648d63f0f11e822319b75e5008a9e4ba179
fortune <omorodion.fortune@gmail.com>
2024-01-04 13:15:35 +0100

    add the search form on header

--- disney-clone/components/Header.tsx
+++ disney-clone/components/Header.tsx
@@ -1,24 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ThemeToggle } from "./ThemeToggler";
+import SearchInput from "./SearchInput";
 function Header() {
   return (
-    <header>
-      <Link href="/">
-        <Image 
-        alt="disney logo"
-        src="https://links.papareact.com/a943ae"
-        width={120}
-        height={100}
-        className="cursor-pointer invert"
+    <header className="fixed flex w-full z-20 top-0 items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+      <Link href="/" className="mr-10">
+        <Image
+          alt="disney logo"
+          src="https://links.papareact.com/a943ae"
+          width={120}
+          height={100}
+          className="cursor-pointer invert-0 dark:invert"
         />
-
-      
       </Link>
       <div className="flex space-x-2 ">
         {/* GenreDropdown */}
-        {/* SearchInput */}
-        {/* Themetoggler */}
+        <SearchInput />
         <ThemeToggle />
       </div>
     </header>
