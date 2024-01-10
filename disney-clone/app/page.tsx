53185ea470b07cae48292e83f3bd657222951352
fortune <omorodion.fortune@gmail.com>
2024-01-10 20:00:16 +0100

    build Movie Carousel

--- disney-clone/app/page.tsx
+++ disney-clone/app/page.tsx
@@ -1,8 +1,20 @@
+import MoviesCarousel from "@/components/MoviesCarousel"
+import { getUpcomingMovies, getTopRatedMovies, getPopularMovies } from "@/lib/getMovies";
+export default async function Home() {
 
-export default function Home() {
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
   return (
     <main>
-    <h1>lets build Disney</h1>
+    
+    {/* carouselBannerWrapper */}
+    <div className="flex flex-col space-y-2 xl:-mt-48">
+      {/* MovieCarousel */}
+      <MoviesCarousel movies={upcomingMovies} title="upcoming" isVertical/>
+      <MoviesCarousel movies={topRatedMovies} title="Top Rated" isVertical/>
+      <MoviesCarousel movies={popularMovies} title="Popular" isVertical/>
+    </div>
     </main>
   )
 }
